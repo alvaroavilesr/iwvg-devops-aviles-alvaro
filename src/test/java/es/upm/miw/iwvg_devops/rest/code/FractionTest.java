c2b28a1eb83eecd40ad67c339bdf5ad425176fca
Alvaro Aviles Redondo <alvaro.aviles2000@gmail.com>
2024-09-17 12:44:08 +0200

    Added tests to class FractionTest #6

--- src/test/java/es/upm/miw/iwvg_devops/rest/code/FractionTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/rest/code/FractionTest.java
@@ -5,18 +5,23 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FractionTest {
 
     Fraction fraction1;
     Fraction fraction2;
+    Fraction fraction3;
+    Fraction fractionEq1;
+    Fraction fractionEq2;
 
     @BeforeEach
     void setUp() {
         fraction1 = new Fraction();
         fraction2 = new Fraction(2,5);
+        fraction3 = new Fraction(4,5);
+        fractionEq1 = new Fraction(5,10);
+        fractionEq2 = new Fraction(10,20);
     }
 
     @Test
@@ -37,5 +42,55 @@ public class FractionTest {
     @DisplayName("Test decimal()")
     void testDecimal() {
         assertEquals((double) 2 / 5, fraction2.decimal());
+    }
+
+    @Test
+    @DisplayName("Test isProper()")
+    void testIsProper() {
+        assertTrue(fraction2.isProper());
+    }
+
+    @Test
+    @DisplayName("Test isImproper()")
+    void testIsImproper() {
+        assertFalse(fraction2.isImproper());
+    }
+
+    @Test
+    @DisplayName("Test isEquivalent()")
+    void testIsEquivalent() {
+        assertTrue(fractionEq1.isEquivalent(fractionEq2));
+    }
+
+    @Test
+    @DisplayName("Test add() with same denominator")
+    void testAddSameDenominator() {
+        Fraction result = fraction2.add(fraction3);
+        assertEquals(6, result.getNumerator());
+        assertEquals(5, result.getDenominator());
+    }
+
+    @Test
+    @DisplayName("Test add() with different denominator")
+    void testAddDifferentDenominator() {
+        Fraction result = fraction1.add(fraction3);
+        assertEquals(9, result.getNumerator());
+        assertEquals(5, result.getDenominator());
+    }
+
+    @Test
+    @DisplayName("Test multiply()")
+    void testMultiply() {
+        Fraction result = fraction2.multiply(fraction3);
+        assertEquals(8, result.getNumerator());
+        assertEquals(25, result.getDenominator());
+    }
+
+    @Test
+    @DisplayName("Test divide()")
+    void testDivide() {
+        Fraction result = fraction2.divide(fraction3);
+        assertEquals(10, result.getNumerator());
+        assertEquals(20, result.getDenominator());
     }
 }
